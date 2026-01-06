234e8654927958f0117e971ef2fc74349a4ea751
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-01-06 14:52:10 +0100

    feat/Add zone, user_base, historique, destinataire & gestionnaire Models

---
+++ app/models/gestionnaire.py
@@ -0,0 +1,4 @@
+from app.models.user_base import User
+
+class Gestionnaire(User) :
+    __tablename__ = 'gestionnaires'
---
+++ app/models/zone.py
@@ -0,0 +1,9 @@
+from sqlalchemy import String, Integer, Column
+from app.models.base import Base
+
+class Zone(Base) :
+    __tablename__ = "zones"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nom = Column(String, nullable=False)
+    code_postal = Column(String, nullable=False)
