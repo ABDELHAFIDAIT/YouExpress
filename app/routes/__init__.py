df6c2814342cd2edb4e1ca08c30c899de6be1aae
abirmes <abirmeskini02@gmail.com>
2026-01-07 16:08:25 +0100

    routes de tout les utilisateurs

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,6 @@
 from .zone_routes import router as zone_router
+from .livreur_routes import router as livreur_route
 
 def include_routes(app):
     app.include_router(zone_router)
+    app.include_router(livreur_route)
